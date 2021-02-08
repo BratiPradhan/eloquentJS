888612462c2459d5f53f06643e93b4a31003bc54
BratiPradhan <pradhanbrati@gmail.com>
2021-02-08 13:42:33 +0530

    CHAPTER-2: EXERCISES
    LOOPING A TRIANGLE, FIZZBUZZ, CHESSBOARD

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('BRATI PRADHAN');
+// LOOPING A TRIANGLE
+for(i=0; i <= 7; i++) {
+    console.log('#'.repeat(i))
+}
+
+// FIZZBUZZ
+let output =''
+for (n=1; n <= 100; n++) {
+    if (n%3 === 0 && n%5 === 0 ) {
+        output = 'FizzBuzz'
+    } else if (n%3 === 0 && n%5 !== 0){
+        output = 'Fizz'
+    } else if (n%3 !== 0 && n%5 === 0) {
+        output = 'Buzz'
+    } else {
+        output = n
+    }
+    console.log(output)
+}
+
+
+
+
+// ChessBoard
+let n = 8
+for (i = 0; i <= n; i++) {
+    if(i%2 !== 0) {
+        console.log(' #'.repeat(n/2))
+    } else {
+        console.log('# '.repeat(n/2))
+    }
+}
